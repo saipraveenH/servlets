905c31aab0ed0d89f71266cfa487563cd6cefddc
sai praveen <hemadripraveen@gmail.com>
2018-05-23 12:06:05 +0530

    Added FirstServlet class

---
+++ src/com/otsi/demo/FirstServlet.java
@@ -0,0 +1,49 @@
+package com.otsi.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class FirstServlet implements Servlet {
+	ServletConfig config = null;
+
+	@Override
+	public void destroy() {
+		System.out.println("servlet destroyed");
+
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return config;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return "This is first servlet created using javax.servlet.Servlet interface";
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		this.config = config;
+		System.out.println("Initializing servlet");
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		out.print("<html><body>");
+		out.print("<b>hello simple servlet</b>");
+		out.print("</body></html>");
+
+	}
+
+}
